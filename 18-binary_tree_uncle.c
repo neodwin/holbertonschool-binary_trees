afa31a26d0941b9718009b012f9ead7af93f0e43
neodwin <edwin.dervaux@gmail.com>
2024-12-11 11:29:39 +0100

    Fix betty style error to 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -5,15 +5,16 @@
   * @node: pointer to the node to find the sibling
   * Return: pointer to the sibling
   */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node)
-                return (NULL);
-        if (node->parent && node->parent->left != node)
-                return (node->parent->left);
-        else if (node->parent && node->parent->right != node)
-                return (node->parent->right);
-        return (NULL);
+	if (!node)
+		return (NULL);
+	if (node->parent && node->parent->left != node)
+		return (node->parent->left);
+	else if (node->parent && node->parent->right != node)
+		return (node->parent->right);
+	return (NULL);
 }
 
 /**
